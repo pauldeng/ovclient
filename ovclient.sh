04e9126805da506dc89aa1cd7b10430b0e0f948b
Paul Deng <paul.deng@gallagher.com>
2022-08-12 13:45:25 +1000

    Applied shellCheck

--- ovclient.sh
+++ ovclient.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-
-die() { #{{{
-	printf "ERROR: $1\n"; exit;
+die() {
+	printf 'ERROR: %s\n' "$1"; exit;
 }
-#}}}
 
-check_status() { #{{{
+check_status() {
 	# As long as ./easyrsa returns 0 (success) we don't bother the non-verbose users
-	should_be_empty=`echo $1 | sed 's/0//g'`
-	[ "X$should_be_empty" == "X" ] || { cat $2; die "Failed for $client" ; }
-	[ "X$VERBOSE" == "X1" ] && { cat $2; }
+	should_be_empty=$(echo "$1" | sed 's/0//g')
+	[ "X$should_be_empty" == "X" ] || { cat "$2"; die "Failed for $client" ; }
+	[ "X$VERBOSE" == "X1" ] && { cat "$2"; }
 }
-#}}}
 
-revoke() { #{{{
-	log=`mktemp`
-	group_name=`groups nobody | cut -f2 -d: | cut -f2 -d' '`
+revoke() {
+	log=$(mktemp)
+	group_name=$(groups nobody | cut -f2 -d: | cut -f2 -d' ')
 	groups nobody | grep -q " $group_name" || { die "Failed at detecting group for user 'nobody'"; }
-	cd /etc/openvpn/server/easy-rsa/
-	./easyrsa --batch revoke "$1" &>> $log
+	cd /etc/openvpn/server/easy-rsa/ || exit
+	./easyrsa --batch revoke "$1" &>> "$log"
 
 	status+=$?;
-	EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl &>> $log
+	EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl &>> "$log"
 	status+=$?;
 	rm -f "/etc/openvpn/server/easy-rsa/pki/private/$1.key" 2>/dev/null
 	rm -f "/etc/openvpn/server/easy-rsa/pki/issued/$1.crt" 2>/dev/null
@@ -32,59 +29,54 @@ revoke() { #{{{
 	chown nobody:"$group_name" /etc/openvpn/server/crl.pem
 	id "$1" &>/dev/null && { userdel "$1"; }
 
-	check_status $status $log 
-	echo "OK! $1 revoked";
+	check_status "$status" "$log"
 }
-#}}}
 
-add() { #{{{
-	log=`mktemp`
+add() {
+	log=$(mktemp)
 	client=$(sed 's/[^0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-]/_/g' <<< "$1")
-	client="vpn_$client"
-	[ -e /etc/openvpn/server/easy-rsa/pki/issued/$client.crt ] && { die "$client exists"; }
-	cd /etc/openvpn/server/easy-rsa/
-	rm -f /etc/openvpn/server/easy-rsa/pki/private/$client.key 2>/dev/null
+	[ -e /etc/openvpn/server/easy-rsa/pki/issued/"$client".crt ] && { die "$client exists"; }
+	cd /etc/openvpn/server/easy-rsa/ || exit
 
-	EASYRSA_CERT_EXPIRE=3650 ./easyrsa build-client-full $client nopass &>> $log
+	EASYRSA_CERT_EXPIRE=3650 ./easyrsa build-client-full "$client" nopass &>> "$log"
 	status+=$?
-	mkdir -p $OVUSERHOME/$client
+	mkdir -p "$OVUSERHOME"/"$client"
+	# Generates the custom client.ovpn
 	{
 	cat /etc/openvpn/server/client-common.txt
 	echo "<ca>"
 	cat /etc/openvpn/server/easy-rsa/pki/ca.crt
 	echo "</ca>"
 	echo "<cert>"
-	sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/$client.crt
+	sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/"$client".crt
 	echo "</cert>"
 	echo "<key>"
-	cat /etc/openvpn/server/easy-rsa/pki/private/$client.key
+	cat /etc/openvpn/server/easy-rsa/pki/private/"$client".key
 	echo "</key>"
 	echo "<tls-auth>"
 	sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/ta.key
 	echo "</tls-auth>"
-	} > $OVUSERHOME/$client/${client}.ovpn
-	chown -R $OVUSER $OVUSERHOME/$client
+	} > "$OVUSERHOME"/"$client"/"${client}".ovpn
+	chown -R "$OVUSER" "$OVUSERHOME"/"$client"
 
-	check_status $status $log 
+	check_status "$status" "$log" 
 	echo "OK! $client created"
 }
-#}}}
 
-list() { #{{{
+list() {
 	[ "X$1" == "Xalphabet" ] && {
-		ls /etc/openvpn/server/easy-rsa/pki/issued/ | grep -v 'server.crt' | while read i; do
-			echo $i | sed 's/\.crt$//'
+		ls /etc/openvpn/server/easy-rsa/pki/issued/ | grep -v 'server.crt' | while read -r i; do
+			echo "$i" | sed 's/\.crt$//'
 		done
 	} 
 	[ "X$1" == "Xdate" ] && {
-		ls -tlh /etc/openvpn/server/easy-rsa/pki/issued/ | grep -v 'server.crt' | while read i; do
-			echo $i | sed 's/\.crt$//'
+		ls -tlh /etc/openvpn/server/easy-rsa/pki/issued/ | grep -v 'server.crt' | while read -r i; do
+			echo "$i" | sed 's/\.crt$//'
 		done
-	} 
+	}
 }
-#}}}
 
-print_help() { #{{{
+print_help() {
 	cat << EOF
 Options:
 -l          list clients by date (ls /etc/openvpn/server/easy-rsa/pki/issued/)
@@ -96,23 +88,23 @@ Options:
 
 EOF
 }
-#}}}
 
 # main {{{
 	[ "X$SUDO_USER" != "X" ] && { OVUSER=$SUDO_USER; } || { OVUSER=$LOGNAME; }
-	OVUSERHOME=$( eval echo ~$OVUSER );
+	OVUSERHOME=$( eval echo ~"$OVUSER" );
 
-	while getopts "lLa:r:gp:vh" opt; do
-		case $opt in
+	while getopts "lLa:r:vh" opt; do
+		case "$opt" in
 			l) list date;;
 			L) list alphabet;;
 			a) ADDCLIENT=$OPTARG ;;
-			r) revoke $OPTARG ;;
+			r) revoke "$OPTARG" ;;
 			v) VERBOSE=1 ;;
 			h) print_help ;;
+			*) echo -n "unknown";;
 		esac
 	done
 	[ -n "$ADDCLIENT" ] && { 
-		add $ADDCLIENT;
+		add "$ADDCLIENT";
 	}
 #}}}
