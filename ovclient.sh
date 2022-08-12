f39b3f5d5db1a2b77d43ca20ea1405d0560a777c
Paul Deng <paul.deng@gallagher.com>
2022-08-12 11:36:09 +1000

    Remove 2FA

--- ovclient.sh
+++ ovclient.sh
@@ -13,6 +13,7 @@ check_status() { #{{{
 	[ "X$VERBOSE" == "X1" ] && { cat $2; }
 }
 #}}}
+
 revoke() { #{{{
 	log=`mktemp`
 	group_name=`groups nobody | cut -f2 -d: | cut -f2 -d' '`
@@ -30,18 +31,17 @@ revoke() { #{{{
 	cp /etc/openvpn/server/easy-rsa/pki/crl.pem /etc/openvpn/server/crl.pem
 	chown nobody:"$group_name" /etc/openvpn/server/crl.pem
 	id "$1" &>/dev/null && { userdel "$1"; }
-	rm "/etc/openvpn/google-authenticator/$1" 2>/dev/null
 
 	check_status $status $log 
 	echo "OK! $1 revoked";
 }
 #}}}
+
 add() { #{{{
 	log=`mktemp`
 	client=$(sed 's/[^0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-]/_/g' <<< "$1")
 	client="vpn_$client"
 	[ -e /etc/openvpn/server/easy-rsa/pki/issued/$client.crt ] && { die "$client exists"; }
-	add_client_google_auth $client
 	cd /etc/openvpn/server/easy-rsa/
 	rm -f /etc/openvpn/server/easy-rsa/pki/private/$client.key 2>/dev/null
 
@@ -69,6 +69,7 @@ add() { #{{{
 	echo "OK! $client created"
 }
 #}}}
+
 list() { #{{{
 	[ "X$1" == "Xalphabet" ] && {
 		ls /etc/openvpn/server/easy-rsa/pki/issued/ | grep -v 'server.crt' | while read i; do
@@ -83,85 +84,6 @@ list() { #{{{
 }
 #}}}
 
-add_client_google_auth() { # {{{
-	cat /etc/openvpn/server/client-common.txt | grep -q '# USE-GOOGLE-AUTHENTICATOR' || { return; }
-	[ "X$GPASSWORD" == "X" ] && { die "Since you enabled Google Authenticator you need to call client.sh -p <password>" ; }
-	mkdir -p $OVUSERHOME/$1
-	useradd --shell=/bin/false --no-create-home $1
-	echo "$1:$GPASSWORD" | chpasswd
-	google-authenticator -l "$1" -t -d -f -r 3 -Q UTF8 -R 30 -w3 -e1 -s /etc/openvpn/google-authenticator/$1 | grep 'https://www.google.com'  > $OVUSERHOME/$1/meta_$1.txt
-	chown gauth:gauth /etc/openvpn/google-authenticator/*
-}
-
-# }}}
-install_google_authenticator () { #{{{
-	apt update
-	apt install -y libpam-google-authenticator
-	cat /etc/group | grep -q gauth ||          { addgroup gauth; }
-	cut -d: -f1 /etc/passwd | grep -q gauth || { useradd -g gauth gauth; }
-	mkdir -p /etc/openvpn/google-authenticator
-	chown gauth:gauth /etc/openvpn/google-authenticator
-	chmod 0700 /etc/openvpn/google-authenticator
-
-	temp=`mktemp`
-	unset PAMPLUGIN
-	unset GAUTH
-
-	[ -e "/usr/lib/openvpn/plugins/openvpn-plugin-auth-pam.so" ] && { 
-		PAMPLUGIN="/usr/lib/openvpn/plugins/openvpn-plugin-auth-pam.so";
-	}
-
-	[ -e "/usr/lib/x86_64-linux-gnu/openvpn/plugins/openvpn-plugin-auth-pam.so" ] && { 
-		PAMPLUGIN="/usr/lib/x86_64-linux-gnu/openvpn/plugins/openvpn-plugin-auth-pam.so";
-	}
-
-	[ -e "/lib/security/pam_google_authenticator.so" ] && { 
-		GAUTH="/lib/security/pam_google_authenticator.so";
-	}
-	[ -e "/lib/x86_64-linux-gnu/security/pam_google_authenticator.so" ] && { 
-		GAUTH="/lib/x86_64-linux-gnu/security/pam_google_authenticator.so";
-	}
-
-	[ "X$PAMPLUGIN" == "X" ] &&   { "Stop. Cannot find /usr/lib/x86_64-linux-gnu/openvpn/plugins/openvpn-plugin-auth-pam.so"; exit; }
-	[ "X$GAUTH" == "X" ] && { "Stop. Cannot find /lib/x86_64-linux-gnu/security/pam_google_authenticator.so";  exit; }
-
-	cat /etc/openvpn/server/server.conf | grep -v "plugin $PAMPLUGIN /etc/pam.d/openvpn" > $temp
-	echo "plugin $PAMPLUGIN /etc/pam.d/openvpn" >> $temp
-	cat $temp > /etc/openvpn/server/server.conf
-
-	echo "auth required $GAUTH secret=/etc/openvpn/google-authenticator/\${USER} user=gauth forward_pass" > /etc/pam.d/openvpn;
-}
-#}}}
-enable_google_authenticator() { #{{{
-	temp=`mktemp`
-	cat /etc/openvpn/server/client-common.txt | grep -v "remote-cert-tls server" | grep -v "auth-nocache" | grep -v "auth-user-pass" | grep -v "reneg-sec" > $temp
-	cat << EOF >> $temp
-reneg-sec 30000			# USE-GOOGLE-AUTHENTICATOR
-remote-cert-tls server	# USE-GOOGLE-AUTHENTICATOR
-auth-nocache			# USE-GOOGLE-AUTHENTICATOR
-auth-user-pass			# USE-GOOGLE-AUTHENTICATOR
-EOF
-	cat $temp > /etc/openvpn/server/client-common.txt
-
-	cat << EOF 
-
-
-
-Google Authenticator is now enabled for future clients. 
-
-To disable Google Authenticator remove these lines from 
-/etc/openvpn/server/client-common.txt:
-
-reneg-sec 30000			# USE-GOOGLE-AUTHENTICATOR 
-remote-cert-tls server	# USE-GOOGLE-AUTHENTICATOR
-auth-nocache			# USE-GOOGLE-AUTHENTICATOR
-auth-user-pass			# USE-GOOGLE-AUTHENTICATOR
-
-EOF
-}
-
-#}}}
-
 print_help() { #{{{
 	cat << EOF
 Options:
@@ -169,8 +91,6 @@ Options:
 -L          list clients by name
 -a <name>   add client
 -r <name>   revoke client
--g          install and enable Google Authenticator
--p          password for Google Authenticator
 -v          be verbose
 -h          this help
 
@@ -188,8 +108,6 @@ EOF
 			L) list alphabet;;
 			a) ADDCLIENT=$OPTARG ;;
 			r) revoke $OPTARG ;;
-			g) install_google_authenticator; enable_google_authenticator ;;
-			p) GPASSWORD=$OPTARG ;;
 			v) VERBOSE=1 ;;
 			h) print_help ;;
 		esac
